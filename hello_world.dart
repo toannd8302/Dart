f8d7bcb8d7bac299ddbc298d7b018c77e3f9158a
Toannd832 <toan03182@gmail.com>
2023-10-04 17:09:54 +0700

    First-time witht Dart programing language

---
+++ hello_world.dart
@@ -0,0 +1,4 @@
+void main(List<String> args) {
+  print("Hello World!");
+  print("This is the firt program with Dart!!!");
+}
---
+++ variable_data_type.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  var nameOFVariable; // Default with Dynamic type
+
+//String
+  String name = 'Toan';
+//Number
+  num age = 21;
+//Boolean
+  bool hasChildren = false;
+  bool hasParent = true;
+//List
+  List informations = ['Toan', '21', 'has parent', 'has not children'];
+//Show data
+  print(name);
+  print(age);
+  print(hasChildren);
+  print(hasParent);
+  print(informations);
+
+//show data with ${variable}
+  print("Show the personal information: This is ${name}, ${age} years old。よろしくお願いします.");
+}
